87563790a1db5ee67b591c0d20e87b9583edb343
JonasImm <jonas.imm@gmail.com>
2020-09-07 18:02:01 +0200

    Add json server script and IDs to db.json

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const jsonServer = require("json-server");
 
 const port = process.env.PORT || 3000;
+const router = jsonServer.router("db.json");
 
+app.use(router);
 // Serve any static files
 app.use(express.static(path.join(__dirname, "client/build")));
 app.use(
