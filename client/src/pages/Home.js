2737747618c265810c27a7b01a536ee0f39f5014
JonasImm <jonas.imm@gmail.com>
2020-09-07 12:01:05 +0200

    Link pages inbetween

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home(props) {
-  return <div>Home</div>;
+  return (
+    <div>
+      <p>Home</p>
+      <Link to="/">
+        <button>Sign out</button>
+      </Link>
+    </div>
+  );
 }
 
 export default Home;
--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Welcome(props) {
   return (
     <div>
       <p>Welcome</p>
-      <button>Klick to sign</button>
+      <Link to="/home">
+        <button>Klick to sign in</button>
+      </Link>
     </div>
   );
 }
