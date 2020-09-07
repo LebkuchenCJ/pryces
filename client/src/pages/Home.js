60f7d80848fa916150e42130707369a9e513bc14
JonasImm <jonas.imm@gmail.com>
2020-09-07 14:20:08 +0200

    Add basic design

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "@emotion/styled";
 
+const Container = styled.div`
+  background-color: #f2f2f2;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  > p {
+    margin: 0;
+  }
+`;
 function Home(props) {
   return (
-    <div>
+    <Container>
       <p>Home</p>
       <Link to="/">
         <button>Sign out</button>
       </Link>
-    </div>
+    </Container>
   );
 }
 
--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -1,14 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  background-color: #f2f2f2;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  > p {
+    margin: 0;
+  }
+`;
 
 function Welcome(props) {
   return (
-    <div>
+    <Container>
       <p>Welcome</p>
       <Link to="/home">
         <button>Klick to sign in</button>
       </Link>
-    </div>
+    </Container>
   );
 }
 
