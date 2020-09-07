801ad566eb86af1d65f9a8b3cf6941ec49fe1f25
JonasImm <jonas.imm@gmail.com>
2020-09-07 17:09:25 +0200

    Add functionality to form and basic CSS

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "@emotion/styled";
 
@@ -7,27 +7,53 @@ const Container = styled.div`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
   flex-direction: column;
   > p {
     margin: 0;
   }
+  > form {
+    display: none;
+    flex-direction: column;
+    position: fixed;
+    border: solid 2px;
+  }
 `;
 function Home() {
+  const [listName, setListName] = useState("");
+
+  function submitName(listName) {
+    console.log(listName);
+  }
+
+  function displayForm() {
+    document.querySelector(".createList").style.display = "flex";
+  }
+  function cancelDisplay() {
+    document.querySelector(".createList").style.display = "none";
+  }
+
   return (
     <Container>
-      <p>Home</p>
-      <Link to="/">
-        <button>Sign out</button>
-      </Link>
+      <header>
+        <p>Home</p>
+        <Link to="/">
+          <button>Sign out</button>
+        </Link>
+      </header>
       <form className="createList">
         <label>Create new shopping list</label>
-        <input type="text" placeholder="Enter shopping list name" />
-        <button>Cancel</button>
-        <button type="submit">Create</button>
+        <input
+          value={listName}
+          placeholder="Enter shopping list name"
+          onChange={(event) => setListName(event.target.value)}
+        />
+
+        <button onClick={() => cancelDisplay()}>Cancel</button>
+        <button onClick={() => submitName(listName)}>Create</button>
       </form>
-      <button>Add List</button>
+      <button onClick={() => displayForm()}>Add List</button>
     </Container>
   );
 }
