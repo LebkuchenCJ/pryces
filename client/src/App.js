c35e43f1dd07514ae51daa98ba9373ce0dd4eddc
JonasImm <jonas.imm@gmail.com>
2020-09-07 11:48:20 +0200

    Add react router and basic pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header"></header>
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/">
+            <Welcome />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Welcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Welcome(props) {
+  return (
+    <div>
+      <p>Welcome</p>
+      <button>Klick to sign</button>
+    </div>
+  );
+}
+
+export default Welcome;
