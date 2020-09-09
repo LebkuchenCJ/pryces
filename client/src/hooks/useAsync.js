975747a6d853a8d7bfc296eb5651d7605a37f369
JonasImm <jonas.imm@gmail.com>
2020-09-09 18:18:13 +0200

    Changed hook and added useCallback and params

--- client/src/hooks/useAsync.js
+++ client/src/hooks/useAsync.js
@@ -1,27 +1,27 @@
-import { useState, useEffect } from "react";
-import { fetchLists } from "../api/list";
+import { useState, useEffect, useCallback } from "react";
 
-function useAsync(asyncFunction) {
+function useAsync(asyncFunction, params) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  const fetchData = async () => {
+  const fetchData = useCallback(async () => {
     try {
       setLoading(true);
       setError(false);
       setData(null);
-      const lists = await fetchLists();
-      setData(lists);
-    } catch (erro) {
+      const newData = await asyncFunction(params);
+      setData(newData);
+    } catch (error) {
+      console.error(error);
       setError(true);
     } finally {
       setLoading(false);
     }
-  };
+  }, [asyncFunction, params]);
   useEffect(() => {
     fetchData();
-  }, [asyncFunction]);
+  }, [fetchData]);
   return { data, loading, error, refetch: fetchData };
 }
 
