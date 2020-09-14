4301fd8bd77935c67d2f35c7bbace92ca68b152c
JonasImm <jonas.imm@gmail.com>
2020-09-14 12:22:20 +0200

    Display supermarkets to page

--- client/src/pages/Compare.js
+++ client/src/pages/Compare.js
@@ -1,19 +1,30 @@
 import React from "react";
 import { fetchSupermarkets } from "../api/supermarkets";
 import Header from "../components/Header";
+import useAsync from "../hooks/useAsync";
 
-function Compare(props) {
+function Compare() {
+  const { data: supermarkets, loading, error } = useAsync(fetchSupermarkets);
+
   async function getSupermarkets() {
     const data = await fetchSupermarkets();
     return console.log(data);
   }
 
   return (
-    <div>
-      <Header title={"Test"} />
-      <p>Test</p>
-      <button onClick={() => getSupermarkets()}>Get markets</button>
-    </div>
+    <>
+      {error && <div>Could not get data. Dont cry. Try again</div>}
+      {loading && <div>Loading...</div>}
+      {supermarkets && (
+        <div>
+          <Header title={"Compare"} />
+          <button onClick={() => getSupermarkets()}>Get markets</button>
+          {supermarkets.map((supermarket) => (
+            <p key={supermarket.id}>{supermarket.name}</p>
+          ))}
+        </div>
+      )}
+    </>
   );
 }
 
