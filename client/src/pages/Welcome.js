47e2c9c60394f5136198bc7c7a42f8726365887b
JonasImm <jonas.imm@gmail.com>
2020-09-16 16:11:08 +0200

    Basic styling to header component

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -8,8 +8,15 @@ function Welcome() {
   return (
     <>
       <Header>
-        <img src={logoSrc} alt="Logo" />
-        <h1>Mega App 2020</h1>
+        <LogoScreen>
+          <img src={logoSrc} alt="Logo" />
+          <h1>Mega App 2020</h1>
+        </LogoScreen>
+
+        <Tabs>
+          <button>Sign In</button>
+          <button>Sign Up</button>
+        </Tabs>
       </Header>
       <Container>
         <h2>Mega App 2020</h2>
@@ -40,13 +47,45 @@ export default Welcome;
 
 const Header = styled.header`
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
   background-image: url(${headerBgSrc});
   background-size: cover;
   background-repeat: no-repeat;
   background-position: bottom;
   height: 30vh;
+`;
+
+const LogoScreen = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-top: 10px;
+  h1 {
+    margin: 0;
+  }
+`;
+
+const Tabs = styled.div`
+  width: 100%;
+  height: 30%;
+  display: flex;
+  align-content: stretch;
+  border-top: #fff solid 3px;
+  > button {
+    width: 100%;
+    background: transparent;
+    border: none;
+    padding: 0;
+    background: rgba(242, 118, 73, 0.5);
+    font-size: 1.6rem;
+    font-weight: bold;
+  }
+  button:first-of-type {
+  }
+  button:last-of-type {
+    color: rgba(0, 0, 0, 0.5);
+  }
 `;
 
 const Container = styled.div`
