17b3ed44cd9ee9975e280095607ae9b9441e38ed
JonasImm <jonas.imm@gmail.com>
2020-09-16 17:23:02 +0200

    Add basic gender select component

---
+++ client/src/components/SignUpForm.js
@@ -0,0 +1,71 @@
+import styled from "@emotion/styled";
+import React from "react";
+import { Link } from "react-router-dom";
+import GenderSelect from "./GenderSelect";
+
+function SignUpForm(props) {
+  return (
+    <Form>
+      <GenderSelect />
+      <label>
+        Name
+        <input placeholder="Enter your name" />
+      </label>
+      <label>
+        E-Mail
+        <input placeholder="Enter your email" />
+      </label>
+      <label>
+        Birthday
+        <input type="date" />
+      </label>
+      <label>
+        Password
+        <input type="password" placeholder="Enter your password" />
+      </label>
+      <label>
+        Confirm Password
+        <input type="password" placeholder="Confirm your password" />
+      </label>
+      <Link to="/home">
+        <input type="submit" placeholder="Klick to sign up" />
+      </Link>
+    </Form>
+  );
+}
+
+export default SignUpForm;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  > label {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 100%;
+  }
+  input {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 2rem;
+    border: solid 1px #fff;
+    border-radius: 20px;
+    background-color: #fff;
+    box-shadow: 3px 3px 6px rgba(0, 0, 0, 0.16);
+  }
+
+  a {
+    width: 100%;
+    margin: 20px;
+  }
+  a > input {
+    border: solid 1px;
+    background-color: #f27649;
+    color: #fff;
+  }
+`;
