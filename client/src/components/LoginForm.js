0f977eaae3ce0c3cae8a6c183c62d85e8a5989d9
JonasImm <jonas.imm@gmail.com>
2020-09-17 12:27:16 +0200

    Added labels to input field and adapted input styling

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -7,11 +7,13 @@ function LoginForm(props) {
   return (
     <Form>
       <label>
+        E-mail
         <div>
           <input placeholder="Enter your E-Mail" />
         </div>
       </label>
       <label>
+        Password
         <div>
           <input placeholder="Enter your password" type="password" />
         </div>
@@ -26,7 +28,8 @@ function LoginForm(props) {
 export default LoginForm;
 
 const Form = styled.form`
-  height: 60vh;
+  height: 60%;
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -38,11 +41,18 @@ const Form = styled.form`
   }
   div > input {
     border: none;
-    margin: 5px;
-    height: 2rem;
+    width: 90%;
+    margin: 0 10px;
+    height: 3rem;
+    background: transparent;
   }
+
+  div > input:focus {
+    outline: none;
+  }
+
   div {
-    border-radius: 20px;
+    border-radius: 30px;
     background-color: var(--bg-color-light);
     box-shadow: 3px 3px 6px rgba(0, 0, 0, 0.16);
   }
