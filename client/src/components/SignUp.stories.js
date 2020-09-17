1e8450a73702fb49a9268a4f92afa5798b31b8c1
JonasImm <jonas.imm@gmail.com>
2020-09-17 14:50:59 +0200

    Create signup story

---
+++ client/src/components/SignUp.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SignUpContainer from "./SignUpContainer";
+import SignUpForm from "./SignUpForm";
+
+export default {
+  title: "SignUpContainer",
+  component: SignUpContainer,
+};
+
+export const Container = () => <SignUpContainer />;
+export const Form = () => <SignUpForm />;
