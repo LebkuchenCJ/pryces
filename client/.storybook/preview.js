fc3f6ca638a967d7fa24e1ff7c624df9f2f93757
JonasImm <jonas.imm@gmail.com>
2020-09-17 11:42:51 +0200

    Add router to preview.js

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,14 +1,18 @@
 import GlobalStyles from "../src/GlobalStyles";
+import { BrowserRouter as Router } from "react-router-dom";
 import React from "react";
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   layout: "fullscreen",
 };
-const withGlobalStyles = (Story, context) => {
+const appDecorator = (Story, context) => {
   return (
     <>
-      <GlobalStyles /> <Story {...context} />
+      <GlobalStyles />
+      <Router>
+        <Story {...context} />
+      </Router>
     </>
   );
 };
-export const decorators = [withGlobalStyles];
+export const decorators = [appDecorator];
