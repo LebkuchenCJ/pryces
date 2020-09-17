de26dac367ef1af07fc3cf79a1def59f0e0416d1
JonasImm <jonas.imm@gmail.com>
2020-09-17 16:05:56 +0200

    Deleted header text

--- client/src/components/SignUpContainer.js
+++ client/src/components/SignUpContainer.js
@@ -5,10 +5,6 @@ import SignUpForm from "./SignUpForm";
 function SignUpContainer(props) {
   return (
     <Container>
-      <div>
-        <h2>MEGA APP 2020</h2>
-        <h3>Sign up and start saving</h3>
-      </div>
       <SignUpForm />
     </Container>
   );
@@ -22,17 +18,4 @@ const Container = styled.div`
   align-items: center;
   flex-direction: column;
   height: 70vh;
-  > div {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-  }
-  h2,
-  h3 {
-    margin: 0;
-  }
-  h2 {
-    color: var(--font-title-welcome);
-    font-size: 2rem;
-  }
 `;
