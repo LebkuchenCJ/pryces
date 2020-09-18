ac488f0512c6712977d2484749d7a0c3dcdb67d9
JonasImm <jonas.imm@gmail.com>
2020-09-18 11:54:47 +0200

    Add text to sign up container

--- client/src/components/SignUpContainer.js
+++ client/src/components/SignUpContainer.js
@@ -5,6 +5,10 @@ import SignUpForm from "./SignUpForm";
 function SignUpContainer(props) {
   return (
     <Container>
+      <div>
+        <h2>MEGA APP 2020</h2>
+        <h3>Sign up and start saving</h3>
+      </div>
       <SignUpForm />
     </Container>
   );
@@ -13,5 +17,22 @@ function SignUpContainer(props) {
 export default SignUpContainer;
 
 const Container = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
   height: 70vh;
+  > div {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+  h3,
+  h2 {
+    margin: 0;
+  }
+  h2 {
+    color: var(--font-title-welcome);
+    font-size: 2rem;
+  }
 `;
