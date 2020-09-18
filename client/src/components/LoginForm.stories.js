bc4cbd98179b50d706935d910f0d9890c0327674
JonasImm <jonas.imm@gmail.com>
2020-09-18 17:14:19 +0200

    Deleted unused stories

--- client/src/components/LoginForm.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import LoginContainer from "./LoginContainer";
-
-export default {
-  title: "LoginContainer",
-  component: LoginContainer,
-};
-
-export const Login = () => <LoginContainer />;
