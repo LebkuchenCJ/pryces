723f0360f60c698c2db51195033eeedf4433e211
JonasImm <jonas.imm@gmail.com>
2020-09-21 15:18:00 +0200

    Create list creation container with first styling

---
+++ client/src/components/ListCreationContainer.js
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+import ListCreationForm from "./ListCreationForm";
+
+function ListCreationContainer({
+  value,
+  onSetName,
+  onHideForm,
+  onHandleSubmit,
+}) {
+  return (
+    <Container>
+      <ListCreationForm
+        value={value}
+        onSetName={onSetName}
+        onHideForm={onHideForm}
+        onHandleSubmit={onHandleSubmit}
+      />
+    </Container>
+  );
+}
+
+export default ListCreationContainer;
+ListCreationContainer.propTypes = {
+  value: PropTypes.string,
+  onSetName: PropTypes.func,
+  onHideForm: PropTypes.func,
+  onHandleSubmit: PropTypes.func,
+};
+
+const Container = styled.div`
+  position: fixed;
+  z-index: 2;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
