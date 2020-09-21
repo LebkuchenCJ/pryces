57048399523f135843c8cc430c00e9082026b0ff
JonasImm <jonas.imm@gmail.com>
2020-09-21 17:34:26 +0200

    Deleted listItemIcon component and added ListItemDeleted.js

---
+++ client/src/components/ListItemDelete.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+import deleteSrc from "../assets/delete_icon.svg";
+import styled from "@emotion/styled";
+
+function ListItemDelete(props) {
+  return (
+    <DeleteButton>
+      <img src={deleteSrc} alt="Delete icon" />
+    </DeleteButton>
+  );
+}
+
+export default ListItemDelete;
+
+ListItemDelete.propTypes = {
+  props: PropTypes.any,
+};
+
+const DeleteButton = styled.button`
+  background-color: transparent;
+  border: none;
+`;
