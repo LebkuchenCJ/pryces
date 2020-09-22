da9b10589c817236760bcdc945cebc4c8276036a
JonasImm <jonas.imm@gmail.com>
2020-09-22 12:53:52 +0200

    Add Product Input overlay

---
+++ client/src/components/ProductAddContainer.js
@@ -0,0 +1,52 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ProductAddForm from "./ProductAddForm";
+import styled from "@emotion/styled";
+
+function ProductAddContainer({
+  onHandleSubmit,
+  query,
+  onSetDisplay,
+  display,
+  onHandleChange,
+  products,
+  onHandleClick,
+}) {
+  return (
+    <Container>
+      <ProductAddForm
+        onHandleSubmit={onHandleSubmit}
+        onSetDisplay={onSetDisplay}
+        display={display}
+        onHandleChange={onHandleChange}
+        products={products}
+        onHandleClick={onHandleClick}
+      />
+    </Container>
+  );
+}
+
+export default ProductAddContainer;
+
+ProductAddContainer.propTypes = {
+  onHandleSubmit: PropTypes.func,
+  query: PropTypes.any,
+  onSetDisplay: PropTypes.func,
+  display: PropTypes.bool,
+  onHandleChange: PropTypes.func,
+  onHandleClick: PropTypes.func,
+  products: PropTypes.any,
+};
+
+const Container = styled.div`
+  position: fixed;
+  z-index: 2;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
---
+++ client/src/components/ProductAddForm.js
@@ -0,0 +1,72 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+
+ProductAddForm.propTypes = {};
+
+function ProductAddForm({
+  onHandleSubmit,
+  query,
+  onSetDisplay,
+  display,
+  onHandleChange,
+  products,
+  onHandleClick,
+}) {
+  return (
+    <Form onSubmit={() => onHandleSubmit}>
+      <label>Add your Products</label>
+      <input
+        placeholder="Search products"
+        value={query}
+        onClick={() => onSetDisplay(!display)}
+        onChange={(event) => {
+          onHandleChange(event.target.value);
+        }}
+      />
+      {display && (
+        <div>
+          {products?.map((product) => (
+            <ProductName
+              key={product.id}
+              onClick={() => {
+                onHandleClick(product);
+              }}
+            >
+              {product.name}
+            </ProductName>
+          ))}
+        </div>
+      )}
+
+      <button>Cancel</button>
+      <input type="submit" value="Add product" />
+    </Form>
+  );
+}
+
+export default ProductAddForm;
+ProductAddForm.propTypes = {
+  onHandleSubmit: PropTypes.func,
+  query: PropTypes.any,
+  onSetDisplay: PropTypes.func,
+  display: PropTypes.bool,
+  onHandleChange: PropTypes.func,
+  onHandleClick: PropTypes.func,
+  products: PropTypes.any,
+};
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  border: solid 1px;
+  margin: 10px 0;
+`;
+
+const ProductName = styled.p`
+  margin: 0;
+  :hover {
+    cursor: pointer;
+    background-color: #f2f2f2;
+  }
+`;
