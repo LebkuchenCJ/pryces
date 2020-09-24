9757f716b20bff0cf86a3ccd3b8b10f14e19576c
JonasImm <jonas.imm@gmail.com>
2020-09-24 15:57:34 +0200

    Deleted unused prop type call

--- client/src/components/ProductAddForm.js
+++ client/src/components/ProductAddForm.js
@@ -4,8 +4,6 @@ import styled from "@emotion/styled";
 import ProductSearchBar from "./ProductSearchBar";
 import ProductSuggestDisplay from "./ProductSuggestDisplay";
 
-ProductAddForm.propTypes = {};
-
 function ProductAddForm({
   onHandleSubmit,
   query,
