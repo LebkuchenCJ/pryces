506eef8c08dafe8be90282c4d66da1cf5ebea807
JonasImm <jonas.imm@gmail.com>
2020-09-28 15:54:01 +0200

    Implement route to server.js and restructred server.js

--- server.js
+++ server.js
@@ -1,57 +1,32 @@
+require("dotenv").config();
 const express = require("express");
+const path = require("path");
+
+const { initDatabase } = require("./lib/database");
+
+const users = require("./lib/routes/list");
+
 const app = express();
-/*   */
-const { MongoClient } = require("mongodb");
-const createListsRouter = require("./routes/list");
-const bodyParser = require("body-parser");
-
 const port = process.env.PORT || 3001;
-/* const router = jsonServer.router("db.json");
-const middleware = jsonServer.defaults(); */
 
-// Serve any static files
-/* app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.json());
+
+app.use("/api/lists", users);
+
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
 app.use(
   "/storybook",
   express.static(path.join(__dirname, "client/storybook-static"))
 );
-app.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-    "/lists/:id": "/lists/:id?_embed=products",
-  })
-);
-app.use(router);
-app.use(middleware); */
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-app.use(bodyParser.json());
-//app.use(cookieParser());
-app.use((request, response, next) => {
-  console.log(`Request ${request.method} on ${request.url}`);
-  next();
-});
-
-const client = new MongoClient(process.env.MONGO_URL, {
-  useUnifiedTopology: true,
-});
-
-async function main() {
-  try {
-    await client.connect();
-  } catch (error) {
-    console.log("Fly, you fools");
+initDatabase(process.env.MONGO_URL, process.env.MONGO_DB_NAME).then(
+  async () => {
+    console.log(`Database ${process.env.MONGO_DB_NAME} is ready`);
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
   }
-
-  const database = client.db(process.env.MONGO_DB_NAME);
-
-  app.use("/api/lists", createListsRouter(database));
-
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-  });
-}
-main();
+);
