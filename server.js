be59006c366f33d668cef848887e14bbce08ff82
JonasImm <jonas.imm@gmail.com>
2020-09-29 15:06:19 +0200

    Implement routes to server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require("path");
 const { initDatabase } = require("./lib/database");
 
 const list = require("./lib/routes/list");
+const products = require("./lib/routes/products");
+const productList = require("./lib/routes/productList");
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -12,6 +14,8 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 
 app.use("/api/lists", list);
+app.use("/api/lists/products", products);
+app.use("/api/productList", productList);
 
 app.use(express.static(path.join(__dirname, "client/build")));
 app.get("*", (req, res) => {
