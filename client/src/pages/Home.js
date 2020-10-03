d7551728b4178c39a8360496806674e6244c0839
JonasImm <jonas.imm@gmail.com>
2020-10-03 14:28:18 +0200

    Display only lists of each user. Temporary solution

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import { fetchLists, postList } from "../api/list";
 import useAsync from "../hooks/useAsync";
@@ -12,8 +12,17 @@ import { deleteList } from "../api/list";
 function Home() {
   const [name, setName] = useState("");
   const [inputfield, setInputfield] = useState(false);
-  const { data: lists, loading, error, refetch } = useAsync(fetchLists);
   const { userId } = sessionStorage;
+  const [userLists, setUserLists] = useState(null);
+  const { data: lists, loading, error, refetch } = useAsync(fetchLists);
+
+  useEffect(() => {
+    function handleLists() {
+      const filteredLists = lists?.filter((list) => list.userId === userId);
+      setUserLists(filteredLists);
+    }
+    handleLists();
+  }, [lists, userId]);
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -38,7 +47,7 @@ function Home() {
         <List>
           {error && <div>Could not get data. Please cry.</div>}
           {loading && <div>Loading...</div>}
-          {lists?.map((list) => (
+          {userLists?.map((list) => (
             <ListItem
               key={list._id}
               list={list}
