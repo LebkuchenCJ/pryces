4bc46be593b90a9d1b3b872abf99c0bdb41119dd
JonasImm <jonas.imm@gmail.com>
2020-10-06 15:47:06 +0200

    Implement pop up form to profile page

--- client/src/components/ProfileDisplay.js
+++ client/src/components/ProfileDisplay.js
@@ -1,25 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
 import passwordSrc from "../assets/lock_icon.svg";
 import mailSrc from "../assets/mail_icon.svg";
 import personSrc from "../assets/person_icon.svg";
+import editSrc from "../assets/edit_icon.svg";
+import ProfileEditDisplay from "./ProfileEditDisplay";
 
 function ProfileDisplay(props) {
+  const [displayEditForm, setDisplayEditForm] = useState(false);
+  const [editDisplayValue, setEditDisplayValue] = useState("");
+
   return (
     <Display>
       <img src={personSrc} alt="Account" />
-      <p>
-        <span>Change your name</span>
-      </p>
+      <button
+        onClick={() => {
+          setEditDisplayValue("name");
+          setDisplayEditForm(true);
+        }}
+      >
+        Change your name
+        <img src={editSrc} alt="Edit" />
+      </button>
       <img src={mailSrc} alt="Mail" />
-      <p>
-        <span>Change your e-mail</span>
-      </p>
+      <button
+        onClick={() => {
+          setEditDisplayValue("e-mail");
+          setDisplayEditForm(true);
+        }}
+      >
+        Change your e-mail
+        <img src={editSrc} alt="Edit" />
+      </button>
       <img src={passwordSrc} alt="Password" />
-      <p>
-        <span>Change your password</span>
-      </p>
+      <button
+        onClick={() => {
+          setEditDisplayValue("password");
+          setDisplayEditForm(true);
+        }}
+      >
+        Change your password
+        <img src={editSrc} alt="Edit" />
+      </button>
+      {displayEditForm && (
+        <ProfileEditDisplay
+          title={editDisplayValue}
+          onCancel={setDisplayEditForm}
+        />
+      )}
     </Display>
   );
 }
@@ -44,23 +73,20 @@ const Display = styled.div`
     justify-self: center;
     align-self: center;
   }
-  > p {
-    display: flex;
+  > button {
     width: 100%;
     height: 3rem;
-    justify-self: center;
-    align-self: center;
-    flex-grow: 2;
+    display: grid;
+    align-items: center;
+    grid-template-columns: 3fr 1fr;
+    border: none;
     border-radius: 30px;
     background-color: var(--bg-color-light);
     box-shadow: 3px 3px 6px rgba(0, 0, 0, 0.16);
   }
-  span {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0 10px;
-    border: none;
-    background: transparent;
+  > button > img {
+    justify-self: center;
+    align-self: center;
+    width: 0.8rem;
   }
 `;
