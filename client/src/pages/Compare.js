7cd7a111227d0defbf63185f580f19854d0aebd2
JonasImm <jonas.imm@gmail.com>
2020-10-08 12:01:39 +0200

    Adapt error and loading handling in compare page

--- client/src/pages/Compare.js
+++ client/src/pages/Compare.js
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 import useAsync from "../hooks/useAsync";
 import PropTypes from "prop-types";
 import SupermarketDisplay from "../components/SupermarketDisplay";
+import EmptyListScreen from "../components/EmptyListScreen";
 import setCompareListsData from "../functions/setCompareListsData";
 import styled from "@emotion/styled";
 
@@ -27,8 +28,10 @@ function Compare({ groceryList }) {
           Your session has ended. Please navigate back to your grocery list.
         </div>
       )}
-      {error && <div>Could not get data. Dont cry. Try again</div>}
-      {loading && <div>Loading...</div>}
+      {error && (
+        <EmptyListScreen text="Could not get data. Dont cry. Try again" />
+      )}
+      {loading && <EmptyListScreen text="Loading..." />}
       {compareListData && (
         <Container>
           {compareListData.map((supermarket) => (
